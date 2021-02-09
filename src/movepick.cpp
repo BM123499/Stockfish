82bff459f6124b71befe51e53dcac9362f2149c4
bmc4 <bmc4@cin.ufpe.br>
2021-02-09 11:53:20 -0300

    Take 8

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -122,9 +122,8 @@ void MovePicker::score() {
                        - (1 << 28);
       }
       else // Type == QUIET_CHECK
-          m.value = - 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
-                    +     (*continuationHistory[0])[make_piece( pos.side_to_move(), KING)][to_sq(m)]
-                    - 2 * (*continuationHistory[0])[make_piece(~pos.side_to_move(), KING)][to_sq(m)];
+          m.value = - (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
+                    - (*continuationHistory[0])[make_piece(~pos.side_to_move(), KING)][to_sq(m)];
 }
 
 /// MovePicker::select() returns the next move satisfying a predicate function.
