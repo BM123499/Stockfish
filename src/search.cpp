9929893059da4518e2f8d42590c2624072741f6c
bmc4 <bmc4@cin.ufpe.br>
2021-02-15 01:29:24 -0300

    Prep for Tuning

--- src/search.cpp
+++ src/search.cpp
@@ -87,7 +87,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return d > 13 ? tw0 : tw1 * d * d + tw2 * d - tw3;
+    return d > 15 ? tw0 : tw1 * d * d + tw2 * d - tw3;
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
