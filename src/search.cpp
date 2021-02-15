2f613ae1fd137d7068acf9d9a93abbcd2d672d1c
bmc4 <bmc4@cin.ufpe.br>
2021-02-15 16:03:43 -0300

    Tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1028,10 +1028,9 @@ moves_loop: // When in check, search starts from here
       // Indicate PvNodes that will probably fail low if node was searched with non-PV search 
       // at depth equal or greater to current depth and result of this search was far below alpha
       bool likelyFailLow =    PvNode 
-                           && ttMove 
+                           && ss->ttHit
                            && (tte->bound() & BOUND_UPPER) 
-                           && ttValue < alpha + 200 + 100 * depth 
-                           && tte->depth() >= depth;
+                           && ttValue < alpha + 200 + 100 * depth;
 
       // Calculate new depth for this move
       newDepth = depth - 1;
@@ -1183,7 +1182,7 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if position is or has been on the PV 
           // and node is not likely to fail low (~10 Elo)
           if (ss->ttPv && !likelyFailLow)
-              r -= 2;
+              r -= 1 + (tte->depth() >= depth);
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if ((rootNode || !PvNode) && thisThread->rootDepth > 10 && thisThread->bestMoveChanges <= 2)
