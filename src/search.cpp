959b215f26736d24f28354647599d14b8b5899cc
bmc4 <bmc4@cin.ufpe.br>
2021-03-02 00:32:44 -0300

    merge ifs

--- src/search.cpp
+++ src/search.cpp
@@ -1467,15 +1467,13 @@ moves_loop: // When in check, search starts from here
     bool pvHit, givesCheck, captureOrPromotion;
     int moveCount;
 
-    if constexpr (PvNode){
+    if (PvNode)
+    {
         alpha = std::max(mated_in(ss->ply), alpha);
         beta = std::min(mate_in(ss->ply+1), beta);
         if (alpha >= beta)
             return alpha;
-    }
 
-    if (PvNode)
-    {
         oldAlpha = alpha; // To flag BOUND_EXACT when eval above alpha and no available moves
         (ss+1)->pv = pv;
         ss->pv[0] = MOVE_NONE;
