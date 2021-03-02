d8029d1f676784393ac9ee1257cf3d289c3a2ea3
bmc4 <bmc4@cin.ufpe.br>
2021-03-01 21:10:50 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -644,7 +644,7 @@ namespace {
         alpha = std::max(mated_in(ss->ply), alpha);
         beta = std::min(mate_in(ss->ply+1), beta);
         if (alpha >= beta)
-            return alpha;
+            return evaluate(pos);
     }
 
     assert(0 <= ss->ply && ss->ply < MAX_PLY);
