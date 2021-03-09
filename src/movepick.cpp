9e824d655cd63bfe6c32c3bf2822e52a7c47bbd1
bmc4 <bmc4@cin.ufpe.br>
2021-03-09 17:39:07 -0300

    fix

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -205,7 +205,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_insertion_sort(cur, endMoves, T1 * depth + T2 * ply);
       }
 
       ++stage;
