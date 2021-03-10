272a9c7ca99e85d517ca4238b90e8fa4f7f587e7
bmc4 <bmc4@cin.ufpe.br>
2021-03-10 18:13:01 -0300

    tweak

--- src/nnue/evaluate_nnue.h
+++ src/nnue/evaluate_nnue.h
@@ -53,7 +53,7 @@ namespace Stockfish::Eval::NNUE {
 
   template <typename T>
   using LargePagePtr = std::unique_ptr<T, LargePageDeleter<T>>;
-
+  extern AlignedPtr<Network> network;
 }  // namespace Stockfish::Eval::NNUE
 
 #endif // #ifndef NNUE_EVALUATE_NNUE_H_INCLUDED
--- src/search.cpp
+++ src/search.cpp
@@ -34,6 +34,7 @@
 #include "tt.h"
 #include "uci.h"
 #include "syzygy/tbprobe.h"
+#include "nnue/evaluate_nnue.h"
 
 namespace Stockfish {
 
@@ -57,6 +58,11 @@ using Eval::evaluate;
 using namespace Search;
 
 namespace {
+
+  constexpr int netbiases[1] = {-229};
+
+  constexpr int netweights[32] = {-27,  -10,  -112,  58, -18,  133,  -120, 25, 24, 59, -18,  21, 10, -17,  -18,  109, 
+  -61,  27, 48, 41, -20,  -26,  17, -27,  -13,  -41,  -17,  -7,  -32,  28, -14,  14};
 
   // Different node types, used as a template parameter
   enum NodeType { NonPV, PV };
@@ -222,6 +228,40 @@ void MainThread::search() {
       nodes = perft<true>(rootPos, Limits.perft);
       sync_cout << "\nNodes searched: " << nodes << "\n" << sync_endl;
       return;
+  }
+
+  if (false)
+  {
+     size_t ndim=Eval::NNUE::Network::kOutputDimensions;
+     std::cout << "  int netbiases[" << ndim << "] = {";
+     for (size_t i=0; i < ndim; ++i)
+     {
+         std::cout << int(Eval::NNUE::network->biases_[i]);
+         if (i < ndim - 1) std::cout << ", ";
+     }
+     std::cout << "}; // int32_t" << std::endl;
+
+     ndim=Eval::NNUE::Network::kOutputDimensions * Eval::NNUE::Network::kPaddedInputDimensions;
+     std::cout << "  int netweights[" << ndim << "] = {";
+     for (size_t i=0; i < ndim; ++i)
+     {
+         std::cout << int(Eval::NNUE::network->weights_[i]);
+         if (i < ndim - 1) std::cout << ", ";
+     }
+     std::cout << "}; // int8_t" << std::endl;
+  }
+  else
+  {
+     size_t ndim=Eval::NNUE::Network::kOutputDimensions;
+     for (size_t i=0; i < ndim; ++i)
+     {
+         Eval::NNUE::network->biases_[i] = netbiases[i];
+     }
+     ndim=Eval::NNUE::Network::kOutputDimensions * Eval::NNUE::Network::kPaddedInputDimensions;
+     for (size_t i=0; i < ndim; ++i)
+     {
+        Eval::NNUE::network->weights_[i] = netweights[i];
+     }
   }
 
   Color us = rootPos.side_to_move();
