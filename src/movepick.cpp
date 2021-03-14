ec243b7b70091ef50ce60e5df0f0272cb119fc10
bmc4 <bmc4@cin.ufpe.br>
2021-03-14 17:06:17 -0300

    tweak

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -111,7 +111,8 @@ void MovePicker::score() {
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   + (ply < MAX_LPH ? std::min(4, depth / 3) * (*lowPlyHistory)[ply][from_to(m)] : 0);
+                   + (ply < MAX_LPH ? std::min(4, depth / 3) * (*lowPlyHistory)[ply][from_to(m)] : 0)
+                   + 1024 * (bool)(pos.blockers_for_king(~pos.side_to_move()) & from_sq(m));
 
       else // Type == EVASIONS
       {
