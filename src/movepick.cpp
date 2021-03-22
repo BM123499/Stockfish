7e6a873ff091cdf105bcdb01e3ab0e3693b51a88
bmc4 <bmc4@cin.ufpe.br>
2021-03-22 12:26:37 -0300

    tweak

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -202,7 +202,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth - 1024 * (ply <= 15));
+          partial_insertion_sort(cur, endMoves, -3000 * depth - 2048 * (ply <= 15));
       }
 
       ++stage;
