e40cc30d53f9476e5ed6e812d22356fa7b0197c3
bmc4 <bmc4@cin.ufpe.br>
2021-04-05 00:05:09 -0300

    remove Knight on condition

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -189,7 +189,7 @@ namespace {
 
         if (!Checks || (Pt != QUEEN && pos.blockers_for_king(~Us) & from))
             b = attacks_bb<Pt>(from, pos.pieces()) & target;
-        else if ((Pt == KNIGHT || Pt == BISHOP) && opposite_colors(from, pos.square<KING>(~Us)))
+        else if (Pt == BISHOP && opposite_colors(from, pos.square<KING>(~Us)))
             continue;
         else
             b = attacks_bb<Pt>(from, pos.pieces()) & target & pos.check_squares(Pt);
