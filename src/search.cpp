d0a009ad8e79f614cc874ad2288f9b63e93831e7
bmc4 <bmc4@cin.ufpe.br>
2021-05-06 19:51:11 -0300

    Test

--- src/search.cpp
+++ src/search.cpp
@@ -1239,13 +1239,6 @@ moves_loop: // When in check, search starts from here
               if (cutNode)
                   r += 2;
 
-              // Decrease reduction for moves that escape a capture. Filter out
-              // castling moves, because they are coded as "king captures rook" and
-              // hence break reverse_move() (~2 Elo)
-              else if (    type_of(move) == NORMAL
-                       && !pos.see_ge(reverse_move(move)))
-                  r -= 2 + ss->ttPv - (type_of(movedPiece) == PAWN);
-
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
