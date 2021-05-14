eba49b90f8d3fafcbd135fee43e2587914370614
bmc4 <bmc4@cin.ufpe.br>
2021-05-14 00:07:40 -0300

    Simplify

--- src/search.cpp
+++ src/search.cpp
@@ -1150,12 +1150,6 @@ moves_loop: // When in check, search starts from here
               && !likelyFailLow)
               r -= 2;
 
-          // Increase reduction at root and non-PV nodes when the best move does not change frequently
-          if (   (rootNode || !PvNode)
-              && thisThread->rootDepth > 10
-              && thisThread->bestMoveChanges <= 2)
-              r++;
-
           // Decrease reduction if opponent's move count is high (~1 Elo)
           if ((ss-1)->moveCount > 13)
               r--;
