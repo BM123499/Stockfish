2b569dd8efb260281914e6b65bf6fac1d3d0e42f
bmc4 <bmc4@cin.ufpe.br>
2021-05-16 10:04:00 -0300

    SImplify

--- src/search.cpp
+++ src/search.cpp
@@ -1013,12 +1013,6 @@ moves_loop: // When in check, search starts from here
           if (   captureOrPromotion
               || givesCheck)
           {
-              // Capture history based pruning when the move doesn't give check
-              if (   !givesCheck
-                  && lmrDepth < 1
-                  && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
-                  continue;
-
               // SEE based pruning
               if (!pos.see_ge(move, Value(-218) * depth)) // (~25 Elo)
                   continue;
