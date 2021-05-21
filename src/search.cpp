86da60fad0c7f27c79e82d9c791353f9f67e9eff
bmc4 <bmc4@cin.ufpe.br>
2021-05-21 10:13:43 -0300

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1101,6 +1101,7 @@ moves_loop: // When in check, search starts from here
       // Check extension (~4 Elo on endgame)
       else if (    givesCheck
                && !checkExtensionDone
+               &&  pos.count<ALL_PIECES>() - pos.count<PAWN>() <= 6
                && (pos.blockers_for_king(~us) & from_sq(move) || pos.see_ge(move)))
           extension = checkExtensionDone = true;
 
