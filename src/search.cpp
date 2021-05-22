00c913efd4f072adf3d3c1bbe4d19b83f96bdbf1
bmc4 <bmc4@cin.ufpe.br>
2021-05-22 09:54:51 -0300

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1097,10 +1097,9 @@ moves_loop: // When in check, search starts from here
                   return beta;
           }
       }
-      else if (    givesCheck
-               &&  move == ttMove
-               &&  pos.count<ALL_PIECES>() - pos.count<PAWN>() <= 6
-               && (pos.blockers_for_king(~us) & from_sq(move) || pos.see_ge(move)))
+      else if (   givesCheck
+               && move == ttMove
+               && pos.count<ALL_PIECES>() - pos.count<PAWN>() <= 6)
           extension = 1;
 
       // Add extension to new depth
