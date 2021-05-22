32e3c4e9d018002eb09c2fe791676731e4023fbe
bmc4 <bmc4@cin.ufpe.br>
2021-05-22 17:20:08 -0300

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1171,6 +1171,8 @@ moves_loop: // When in check, search starts from here
               if (!ss->inCheck)
                   r -= ss->statScore / 14721;
           }
+          else if (type_of(move) == NORMAL && !pos.see_ge(move))
+              r++;
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
