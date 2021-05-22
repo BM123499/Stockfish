ccae7a093c39b7326628e2a7d252b229c4e713ce
bmc4 <bmc4@cin.ufpe.br>
2021-05-22 03:09:01 -0300

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1101,7 +1101,7 @@ moves_loop: // When in check, search starts from here
       // Check extension (~4 Elo on endgame)
       else if (    givesCheck
                &&  move == ttMove
-               &&  pos.count<ALL_PIECES>() - pos.count<PAWN>() <= 7
+               &&  pos.count<ALL_PIECES>() - pos.count<PAWN>() <= 5
                && (pos.blockers_for_king(~us) & from_sq(move) || pos.see_ge(move)))
           extension = 1;
 
