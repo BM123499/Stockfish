3c7dfb74cad3c9301fdf206ce480bc76620e7eb9
bmc4 <bmc4@cin.ufpe.br>
2021-05-28 01:00:30 -0300

    test

--- src/search.cpp
+++ src/search.cpp
@@ -56,7 +56,7 @@ using std::string;
 using Eval::evaluate;
 using namespace Search;
 
-int T[7] = {209, 44, 218, 174, 157, 28255, 537};
+int T[7] = {221, 51, 235, 138, 151, 28243, 556};
 TUNE(T);
 
 namespace {
