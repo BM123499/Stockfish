7d733e3d11124f56721fa44922a698833f06152b
bmc4 <bmc4@cin.ufpe.br>
2021-05-28 10:24:59 -0300

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1120,6 +1120,7 @@ moves_loop: // When in check, search starts from here
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
+          &&  pos.non_pawn_material(us)
           && (  !captureOrPromotion
               || (cutNode && (ss-1)->moveCount > 1)
               || (!PvNode && !formerPv))
