059cd89660a4dfecdc14dfe5d862b602ca7a8c0b
bmc4 <bmc4@cin.ufpe.br>
2021-06-08 11:03:31 -0300

    test
    bench: 4026911

--- src/search.cpp
+++ src/search.cpp
@@ -1135,6 +1135,7 @@ moves_loop: // When in check, search starts from here
           if (PvNode)
               r--;
 
+
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
               r--;
@@ -1179,6 +1180,8 @@ moves_loop: // When in check, search starts from here
               if (!ss->inCheck)
                   r -= ss->statScore / 14721;
           }
+
+          r += moveCount / 8;
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
