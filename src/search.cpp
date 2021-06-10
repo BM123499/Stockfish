ab10d8120b324dd554bb5020764acda8eecc9c37
bmc4 <bmc4@cin.ufpe.br>
2021-06-10 14:22:23 -0300

    test
    bench: 4092149

--- src/search.cpp
+++ src/search.cpp
@@ -1508,7 +1508,7 @@ moves_loop: // When in check, search starts from here
 
       // Do not search moves with negative SEE values
       if (    bestValue > VALUE_TB_LOSS_IN_MAX_PLY
-          && !pos.see_ge(move))
+          && !pos.see_ge(move, VALUE_ZERO + 1))
           continue;
 
       // Speculative prefetch as early as possible
