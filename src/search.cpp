d04e40971ea2acd4cba0bbdcf07607ba9c3ce041
bmc4 <bmc4@cin.ufpe.br>
2021-06-11 10:10:10 -0300

    test
    bench: 4153027

--- src/search.cpp
+++ src/search.cpp
@@ -1169,6 +1169,9 @@ moves_loop: // When in check, search starts from here
               if (ttCapture)
                   r++;
 
+              if (!improving)
+                  r++;
+
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
